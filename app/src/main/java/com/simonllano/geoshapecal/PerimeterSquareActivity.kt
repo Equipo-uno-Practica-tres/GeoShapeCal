1d000545da210b5f9f97428540f890362e87ce27
simonllano <77639502+simonllano@users.noreply.github.com>
2023-09-18 21:12:22 -0500

    Commit inicial Practica 3

---
+++ app/src/main/java/com/simonllano/geoshapecal/PerimeterSquareActivity.kt
@@ -0,0 +1,11 @@
+package com.simonllano.geoshapecal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerimeterSquareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perimeter_square)
+    }
+}
